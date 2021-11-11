5d5374d6d300982cd7a51a357319e257340ef208
Sacdeneu <sacdeneu@gmail.com>
2021-11-11 15:32:39 -0500

    Home screen List added

--- app/src/main/java/com/example/spire/MainActivity.kt
+++ app/src/main/java/com/example/spire/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.spire
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_list)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menulist, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
