7bf4f5da160d8a8a1f572fc19ed85e5728e94fb3
Sacdeneu <sacdeneu@gmail.com>
2021-11-11 17:07:26 -0500

    Fragments Home menu and Settings menu added

--- app/src/main/java/com/example/spire/MainActivity.kt
+++ app/src/main/java/com/example/spire/MainActivity.kt
@@ -5,14 +5,53 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.example.spire.fragments.HomeFragment
+import com.example.spire.fragments.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.annotation.NonNull
+
+import com.google.android.material.navigation.NavigationBarView
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+        //initialise fragments
+        val homeFragment = HomeFragment()
+        val settingsFragment = SettingsFragment()
+
+        //setup toolbar
         val toolbar: Toolbar = findViewById(R.id.toolbar_list)
         setSupportActionBar(toolbar)
+        replaceFragment(homeFragment)
+
+        //listener pour les boutons du menu du bas
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.ic_home -> replaceFragment(homeFragment)
+                R.id.ic_settings -> replaceFragment(settingsFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        if(fragment != null){ //si le fragment n'est pas null on le remplace quand on change d'écran dans le menu du bas
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container, fragment)
+                commit()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
