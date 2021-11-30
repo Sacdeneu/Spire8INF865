0bb0ceecbe21dc48df4987e14c67fd6533468ae8
Zelef973 <basile.talmon@outlook.fr>
2021-11-30 15:13:20 -0500

    modif gradel

--- app/src/main/java/com/example/spire/RegisterActivity.kt
+++ app/src/main/java/com/example/spire/RegisterActivity.kt
@@ -2,10 +2,17 @@ package com.example.spire
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
+private var mAuth: FirebaseAuth? = null
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
+
 }
