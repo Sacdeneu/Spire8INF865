cb3d5d4e2198303dc6463305e03c102d1f3021c1
Sacdeneu <sacdeneu@gmail.com>
2021-12-03 20:17:12 -0500

    Déconnexion implémentée dans les paramètres.

--- app/src/main/java/com/example/spire/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/spire/fragments/SettingsFragment.kt
@@ -1,46 +1,39 @@
 package com.example.spire.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 import androidx.preference.*
+import com.example.spire.LoginActivity
+import com.example.spire.MainActivity
 import com.example.spire.R
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.core.utilities.Utilities
+import com.google.firebase.ktx.Firebase
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
+
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.prefs)
-
-        val notificationPreference: CheckBoxPreference? = findPreference("DarkMode")
-        notificationPreference!!.onPreferenceChangeListener =
-            Preference.OnPreferenceChangeListener { preference, newValue ->
-                // Manually save to Prefs
-                val sh = requireActivity().applicationContext.getSharedPreferences("DarkMode", Context.MODE_PRIVATE).edit();
-                if(newValue == true) {
-                    sh.putBoolean("is",true);
-                    sh.commit();
-                    val ft = requireFragmentManager().beginTransaction()
-                    ft.detach(this).attach(this).commit()
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                }
-                else {
-                    sh.putBoolean("is",false);
-                    sh.commit();
-                    val ft = requireFragmentManager().beginTransaction()
-                    ft.detach(this).attach(this).commit()
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                }
-
-                // Reflect the newValue to Preference?
-                true
+        val disconnectPref = findPreference("disconnect") as Preference?
+        disconnectPref!!.setOnPreferenceClickListener(object :
+            Preference.OnPreferenceClickListener {
+            override fun onPreferenceClick(preference: Preference?): Boolean {
+                Firebase.auth.signOut()
+                val logOutintent= Intent(activity, LoginActivity::class.java)
+                startActivity(logOutintent)
+                activity?.finish()
+                return true
             }
-
+        })
     }
 
 
