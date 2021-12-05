cfaac1b26e8c67bd61e92ea88b82838b3a6e39e2
Sacdeneu <sacdeneu@gmail.com>
2021-12-05 18:40:05 -0500

    Hotfix searchfragment

--- app/src/main/java/com/example/spire/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/spire/fragments/SearchFragment.kt
@@ -14,7 +14,6 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.example.spire.PlaceHolderApi1
 import com.example.spire.R
 import com.example.spire.databinding.FragmentSearchBinding
 import org.json.JSONArray
